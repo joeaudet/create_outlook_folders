ace63b4cbd522d076cb0e36897aab249990b45fb
Joe Audet <jaudet@ans-llc.net>
2018-08-13 09:03:25 -0400

    Create create_outlook_folders.ps1
    Initial commit of working script to create 'Mailing Lists' folder and subsequent subfolders

---
+++ create_outlook_folders.ps1
@@ -0,0 +1,48 @@
+$comments = @' 
+Script name: Create-Folder.ps1 
+Created on: Wednesday, September 26, 2007 
+Author: Kent Finkle 
+Original Purpose: How can I use Windows Powershell to Create a New Folder in Microsoft Outlook?
+Modified: 2018AUG010 Joe Audet
+Current Purpose: Create a subfolder structure for mailing lists, ignoring folders that are already created
+'@ 
+#----------------------------------------------------- 
+#Set the Inbox folder
+$olFolderInbox = 6
+
+#Load the Outlook COM object and set the namespace
+$o = new-object -comobject outlook.application 
+$n = $o.GetNamespace("MAPI") 
+
+#Get the Inbox and store it as an object
+$inbox = $n.GetDefaultFolder($olFolderInbox)
+
+#Initial revision uses an array of folder names to create subfolders under the 'Mailing Lists' folder within the Inbox
+$mailing_lists_array = @("SUB_FOLDER_1","SUB_FOLDER_2")
+
+#Check if the 'Mailing Lists' folder exists - If yes, send a message and do nothing, if not create the folder
+if ([bool]($mailing_list_folder = $inbox.Folders | where-object { $_.name -eq "Mailing Lists" }) ) {
+	Write-Host "Folder: $($inbox.name)\$($mailing_list_folder.name) Already Exists - Skipping"
+} else {
+	$newfolder = $inbox.Folders.Add("Mailing Lists")
+	Write-Host "$($newfolder.name) Created"
+	$mailing_list_folder = $inbox.Folders | where-object { $_.name -eq "Mailing Lists" }
+}
+
+#Function to iterate through the array, checking to see if each folder exists - if so send a message and do nothing, if not create folder
+function create_subfolders {
+	foreach ($array_folder in $mailing_lists_array) {
+		if ( [bool]( $targetfolder = $mailing_list_folder.Folders | where-object { $_.name -eq $array_folder } ) ) {
+			Write-Host "Folder: $($array_folder) exists under $($inbox.name)\$($mailing_list_folder.name) - Skipping"
+		} else {
+			$newfolder = $mailing_list_folder.Folders.Add($array_folder)
+			Write-Host "Created folder: $($array_folder) under $($inbox.name)\$($mailing_list_folder.name)"
+		}
+	}
+}
+
+#Call function to create folders
+create_subfolders
+
+#Send message to let user know folders were created
+Write-Host "Create / Update Mailing List folder script complete"
